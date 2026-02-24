8c36df9fc1444075c27d630acfb35d671de3aa71
Myeongkeun Jeong <mkjmk3114@gmail.com>
2026-02-25 00:59:07 +0900

    style: change shuttle link text to simple '홈페이지 이동'

--- app.js
+++ app.js
@@ -966,7 +966,7 @@ function showCenterInfo(fcCode, center) {
 
     shuttleLink.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg>
-        셔틀 홈페이지
+        홈페이지 이동
     `;
 
     // Naver Maps search
